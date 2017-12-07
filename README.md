707df67aafce1e8683f604e46765890a6e6430b1
oreqizer <oreqizer@gmail.com>
2017-12-07 12:29:10 +0100

    INIT

---
+++ README.md
@@ -0,0 +1,3 @@
+# 3dpp
+
+A project I am learning 3D graphics on in C++ following [this](https://www.youtube.com/watch?v=ftiKrP3gW3k&index=1&list=PLEETnX-uPtBXT9T-hD0Bj31DSnwio-ywh) video tutorial. Doing so on Mac.
---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <GL/glew.h>
+// #include <vulkan/vulkan.h>
+
+#define WIDTH 800
+#define HEIGHT 600
+
+// Make configs:
+//   brew install cmake
+//   cmake -H. -Bbuild
+// Compile
+//   cmake --build build
+int main() {
+    return 0;
+}
