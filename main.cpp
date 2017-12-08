d35b0e02fa150346bdfe69ab9581813a730978bd
oreqizer <oreqizer@gmail.com>
2017-12-08 12:51:34 +0100

    main: Base code finish

--- main.cpp
+++ main.cpp
@@ -5,26 +5,52 @@
 #include <stdexcept>
 #include <functional>
 
+const int WIDTH = 800;
+const int HEIGHT = 600;
+
 class HelloTriangleApplication {
 public:
     void Run() {
+        initWindow();
         initVulkan();
         mainLoop();
         cleanup();
     }
 
 private:
+    void initWindow() {
+        glfwInit();
+
+        // tells GLFW not to enable OpenGL context
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+        // no need to resize window for now
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+        m_window = glfwCreateWindow(
+                WIDTH,
+                HEIGHT,
+                "Vulkan",
+                nullptr, // monitor to open the window on
+                nullptr  // something for OpenGL
+        );
+    }
+
     void initVulkan() {
 
     }
 
     void mainLoop() {
-
+        while (!glfwWindowShouldClose(m_window)) {
+            glfwPollEvents();
+        }
     }
 
     void cleanup() {
-
+        glfwDestroyWindow(m_window);
+        glfwTerminate();
     }
+
+    GLFWwindow* m_window;
 };
 
 int main() {
