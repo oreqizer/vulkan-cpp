4c986e54f851719b2c1cca3316c2d0581630f25d
oreqizer <oreqizer@gmail.com>
2017-12-10 21:47:11 +0100

    pipeline: Add subpass dependency

--- src/pipeline.cpp
+++ src/pipeline.cpp
@@ -56,12 +56,23 @@ VkRenderPass pipeline::createRenderPass(VkDevice device, VkFormat format) {
             .pColorAttachments = &colorAttachmentRef,
     };
 
+    VkSubpassDependency dependency = {
+            .srcSubpass = VK_SUBPASS_EXTERNAL,
+            .dstSubpass = 0,
+            .srcStageMask = VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT,
+            .srcAccessMask = 0,
+            .dstStageMask = VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT,
+            .dstAccessMask = VK_ACCESS_COLOR_ATTACHMENT_READ_BIT | VK_ACCESS_COLOR_ATTACHMENT_WRITE_BIT,
+    };
+
     VkRenderPassCreateInfo renderPassInfo = {
             .sType = VK_STRUCTURE_TYPE_RENDER_PASS_CREATE_INFO,
             .attachmentCount = 1,
             .pAttachments = &colorAttachment,
             .subpassCount = 1,
             .pSubpasses = &subpass,
+            .dependencyCount = 1,
+            .pDependencies = &dependency,
     };
 
     VkRenderPass renderPass;
